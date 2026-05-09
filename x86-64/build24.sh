e43d87d000851504057b1da976ea1c74b15f56ce
omg7789 <59000287+omg7789@users.noreply.github.com>
2026-05-10 00:13:03 +0800

    Update build24.sh

--- x86-64/build24.sh
+++ x86-64/build24.sh
@@ -86,7 +86,7 @@ PACKAGES="$PACKAGES kmod-usb-net"
 
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
-# ======== shell/custom-packages.sh =======
+ ======== shell/custom-packages.sh =======
 # 合并imm仓库以外的第三方插件
 PACKAGES="$PACKAGES $CUSTOM_PACKAGES"
 
